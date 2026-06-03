7ee0249fdb944440369f6a8ee4456404932f6c68
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-06-03 12:03:23 -0700

    Completed size() method

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -108,7 +108,10 @@ public class TraversalPractice {
      * @return the number of nodes in the tree
      */
     public static int size(TreeNode node) {
-        return -1;
+        if(node == null) return 0;
+        int leftNode = size(node.left);
+        int rightNode = size(node.right);
+        return leftNode + rightNode + 1;
     }
 
     /**

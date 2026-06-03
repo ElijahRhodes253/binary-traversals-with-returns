2ba759daac13fc6276610521f8a2d10833205d93
Elijah_Rhodes <mastergamer3dr@gmail.com>
2026-06-03 12:08:32 -0700

    Completed branchCount() method

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -138,7 +138,14 @@ public class TraversalPractice {
      * @return the count of branch nodes in the tree
      */
     public static int branchCount(TreeNode node) {
-        return -1;
+        if(node == null) return 0;
+        if(node.left == null && node.right == null)
+        {
+            return 0;
+        }
+        int leftNode = branchCount(node.left);
+        int rightNode = branchCount(node.right);
+        return leftNode + rightNode + 1;
     }
 
 
